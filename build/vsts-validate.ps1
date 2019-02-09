f24a044b3b87827a9324a5f1bfdb0547e5cd81aa
Andi Bellstedt <bellstedt@gmx.de>
2019-02-09 18:46:46 +0100

    install module dependency 'BurnToast'

--- build/vsts-validate.ps1
+++ build/vsts-validate.ps1
@@ -2,6 +2,7 @@
 # https://docs.microsoft.com/en-us/vsts/build-release/concepts/definitions/build/variables?tabs=powershell
 
 # Needs to ensure things are Done Right and only legal commits to master get built
+Find-Module -Name BurnToast -MinimumVersion '0.6.3' -IncludeDependencies -Confirm:$false | Install-Module -Scope CurrentUser -Force -Confirm:$false
 
 # Run internal pester tests
 & "$PSScriptRoot\..\PackageUpdateInfo\tests\pester.ps1"
