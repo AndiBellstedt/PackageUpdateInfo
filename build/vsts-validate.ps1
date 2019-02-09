d1df3d36877cea35587302d4207c78980a03f5b1
Andi Bellstedt <bellstedt@gmx.de>
2019-02-09 18:57:05 +0100

    bugfix module dependecy installation

--- build/vsts-validate.ps1
+++ build/vsts-validate.ps1
@@ -2,7 +2,11 @@
 # https://docs.microsoft.com/en-us/vsts/build-release/concepts/definitions/build/variables?tabs=powershell
 
 # Needs to ensure things are Done Right and only legal commits to master get built
-Find-Module -Name BurnToast -MinimumVersion '0.6.3' -IncludeDependencies -Confirm:$false | Install-Module -Scope CurrentUser -Force -Confirm:$false
+Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+
+$requiredModules = (Import-PowerShellDataFile -Path "$($PSScriptRoot)\..\PackageUpdateInfo\PackageUpdateInfo.psd1").RequiredModules
+Write-Verbose $requiredModules -Verbose
+Find-Module -Name "BurntToast" -MinimumVersion '0.6.3' | Install-Module -Scope CurrentUser -Force -Confirm:$false
 
 # Run internal pester tests
 & "$PSScriptRoot\..\PackageUpdateInfo\tests\pester.ps1"
