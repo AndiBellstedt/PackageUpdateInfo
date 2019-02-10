a9bad32fca2d0a017c28d3e47b02931571a36b05
Andi Bellstedt <bellstedt@gmx.de>
2019-02-10 09:00:16 +0100

    add rereq checks/install on required modules in CI pipeline

--- build/vsts-validate.ps1
+++ build/vsts-validate.ps1
@@ -2,10 +2,23 @@
 # https://docs.microsoft.com/en-us/vsts/build-release/concepts/definitions/build/variables?tabs=powershell
 
 # Needs to ensure things are Done Right and only legal commits to master get built
-Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
 
-$requiredModules = (Import-PowerShellDataFile -Path "$($PSScriptRoot)\..\PackageUpdateInfo\PackageUpdateInfo.psd1").RequiredModules
-Find-Module -Name "BurntToast" -MinimumVersion '0.6.3' | Install-Module -Scope CurrentUser -Force -Confirm:$false -Verbose
+# Check requirements for other modules and install them first
+Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+$requiredModules = (Import-PowerShellDataFile -Path "$PSScriptRoot\..\PackageUpdateInfo.psd1").RequiredModules
+foreach ($requiredModule in $requiredModules) {
+    $moduleParams = @{
+        Name = $requiredModule['ModuleName']
+    }
+    if($requiredModule['RequiredVersion']) {
+        $moduleParams.Add("RequiredVersion",$requiredModule['RequiredVersion'])
+        Write-Host "Install required module: $($requiredModule['ModuleName']) (RequiredVersion: v$($requiredModule['RequiredVersion']))"
+    } else {
+        $moduleParams.Add("MinimumVersion",$requiredModule['ModuleVersion'])
+        Write-Host "Install required module: $($requiredModule['ModuleName']) (MinimumVersion: v$($requiredModule['ModuleVersion']))"
+    }
+    Find-Module @moduleParams | Install-Module -Scope CurrentUser -Force -Confirm:$false
+}
 
 # Run internal pester tests
 & "$PSScriptRoot\..\PackageUpdateInfo\tests\pester.ps1"
