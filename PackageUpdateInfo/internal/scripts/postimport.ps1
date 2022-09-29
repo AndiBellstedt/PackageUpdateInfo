f54975204fba9e1e3bbc1005ddd9d3470a2b1ef9
Andi Bellstedt <bellstedt@gmx.de>
2022-09-29 22:19:35 +0200

    code refactoring to enable publish

--- PackageUpdateInfo/internal/scripts/postimport.ps1
+++ PackageUpdateInfo/internal/scripts/postimport.ps1
@@ -1,25 +1,14 @@
-﻿# Place all code that should be run after functions are imported here
+﻿<#
+Add all things you want to run after importing the main function code
 
-$script:ModuleIconPath = Join-Path -Path $script:ModuleRoot -ChildPath "\bin\PackageUpdateInfo.png"
+WARNING: ONLY provide paths to files!
 
-if ($isLinux) {
-    $script:ModuleTempPath = Join-Path -Path "/tmp" -ChildPath "PackageUpdateInfo"
-    $script:ModuleSettingPath = Join-Path -Path $HOME -ChildPath ".local/share/powershell/PackageUpdateInfo/PackageUpdateSetting.json"
-} else {
-    $script:ModuleTempPath = Join-Path -Path $env:TEMP -ChildPath "PackageUpdateInfo"
-    $script:ModuleSettingPath = Join-Path -Path $HOME -ChildPath "AppData\Local\Microsoft\Windows\PowerShell\PackageUpdateSetting.json"
-}
+After building the module, this file will be completely ignored, adding anything but paths to files ...
+- Will not work after publishing
+- Could break the build process
+#>
 
-$script:CurrentUserModulePath = $env:PSModulePath.split(';') | Where-Object { $_ -like "$(Split-Path $PROFILE -Parent)*" -or $_ -like "$($HOME)*" }
-$script:AllUsersModulePath = $env:PSModulePath.split(';') | Where-Object { $_ -notlike "$(Split-Path $PROFILE -Parent)*" -and $_ -notlike "$($HOME)*" }
+$moduleRoot = Split-Path (Split-Path $PSScriptRoot)
 
-if (Test-Path -Path $script:ModuleTempPath) {
-    Get-ChildItem -Path $script:ModuleTempPath | Where-Object LastWriteTime -lt (Get-Date).AddHours(-2) | Remove-Item -Force -Confirm:$false
-} else {
-    New-Item -Path $script:ModuleTempPath -ItemType Directory -Force | Out-Null
-}
-
-if (-not (Test-Path -Path $script:ModuleSettingPath)) {
-    Write-Verbose -Message "Going to initialize default settings for module PackageUpdateInfo"
-    Set-PackageUpdateSetting -Reset -Path $script:ModuleSettingPath
-}
+# Init defaults
+"$moduleRoot\internal\scripts\InitDefaults.ps1"
